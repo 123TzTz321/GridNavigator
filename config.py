3265215cad96117ac263be551c989cb9bed03340
Thomas <tztz@stenon.io>
2021-10-17 16:31:55 +0200

    add config parser

---
+++ config.py
@@ -0,0 +1,14 @@
+from configparser import ConfigParser
+
+def config(filename='db.ini', section=''):
+    parser = ConfigParser()
+    parser.read(filename)
+    conf=[]
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            conf[param[0]] = param[1]
+    else:
+        raise Exception(f'Section {section} not found in the {filename} file')
+
+    return conf
